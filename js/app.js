bb85928a11f8754396c6c239cc9d28d90de69155
Maria Delacruz <mar23iaa@gmail.com>
2024-01-26 15:13:19 -0800

    Added a constructor and two functions

--- js/app.js
+++ js/app.js
@@ -1,4 +1,19 @@
 class App {
+  constructor() {
+    // We are using $form because it's an HTML element
+    this.$form = document.querySelector('#form')
+    this.addEventListeners();
+  }
+
+  addEventListeners() {
+    document.body.addEventListener('click', event => {
+      this.handleFormClick(event);
+    })
+  }
+
+  handleFormClick(event) {
+
+  }
 }
 
 new App()
